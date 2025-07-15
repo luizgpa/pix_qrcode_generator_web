b8ca740485c28334d65f0625712e65e192a82dc4
Luiz Guilherme Alvarez <luizgpa@gmail.com>
2025-07-15 16:05:00 -0300

    Icon changed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2057112389"
+    serviceWorkerVersion: "3215722991"
   }
 });
